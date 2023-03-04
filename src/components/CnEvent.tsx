1fd292bbae3343276e83ac2a1fce9131e2b7b6ad
Stephen Ouma <stephen.ouma@squaddigital.com>
2023-03-04 14:50:54 +0300

    updates

--- src/components/CnEvent.tsx
+++ src/components/CnEvent.tsx
@@ -8,7 +8,13 @@ interface Props {
 }
 
 const CnEvent: React.FC<Props> = ({ item, currentLocation }) => {
-    return <div className="item">{`Item - ${item.name}` }</div>;
+    return (
+        <div className="item-detail">
+            <h1>{`Item - ${item.name}`}</h1>
+            <h3>Events</h3>
+            <p>{`Current location - ${currentLocation}`}</p>
+        </div>
+    );
 };
 
 export default CnEvent;
